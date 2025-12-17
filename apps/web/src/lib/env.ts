8d15df4ea1b0eda36f712725c2118ab89a9c337e
Just.Chola <simmonsfrank@gmail.com>
2025-12-17 17:28:15 +0200

    select correct API_BASE_URL depending on BUN_ENV

--- apps/web/src/lib/env.ts
+++ apps/web/src/lib/env.ts
@@ -3,9 +3,10 @@
  * Add to `vite.config.ts` `define` or use `$env/static/public`
  */
 const API_BASE_URL =
-	import.meta.env.API_BASE_URL || import.meta.env.NODE_ENV === 'development'
+	import.meta.env.API_BASE_URL ||
+	(import.meta.env.BUN_ENV === 'development'
 		? 'http://localhost:3000' // ← your Elysia dev port
-		: 'https://api.plates.simmons.studio';
+		: 'https://api.plates.simmons.studio');
 
 if (!API_BASE_URL) {
 	throw new Error('PUBLIC_API_BASE_URL is required');
